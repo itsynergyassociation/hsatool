db99e474390f0cf15ff5d61905d390e5d51c8f2a
itsynergyassociation <118040795+itsynergyassociation@users.noreply.github.com>
2022-11-23 13:13:23 +0500

    Update settings.py

--- RNN_W_Django/settings.py
+++ RNN_W_Django/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
-STATIC_ROOT = os.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
