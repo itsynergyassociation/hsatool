b78f77148a4cdd24c59683e913995e216f49a348
itsynergyassociation <118040795+itsynergyassociation@users.noreply.github.com>
2022-11-25 15:49:20 +0500

    Update settings.py

--- RNN_W_Django/settings.py
+++ RNN_W_Django/settings.py
@@ -59,10 +59,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'RNN_W_Django.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+ #   'default': {
+ #      'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #   }
 }
 
 
