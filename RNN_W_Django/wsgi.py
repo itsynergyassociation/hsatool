2cd6505165736541b757e12f4d2d8dd811ad2f17
itsynergyassociation <118040795+itsynergyassociation@users.noreply.github.com>
2022-11-25 17:01:50 +0500

    Update wsgi.py

--- RNN_W_Django/wsgi.py
+++ RNN_W_Django/wsgi.py
@@ -14,4 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RNN_W_Django.settings')
 
 application = get_wsgi_application()
-app = application
+
